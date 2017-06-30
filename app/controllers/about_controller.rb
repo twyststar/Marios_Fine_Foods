10a797e3e030c63e6018a33280cca3cd2a300dd5
Starlynne <lewis.start0905@gmail.com>
2017-06-30 16:27:45 -0700

    Add header, navbar, styling, about page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+  def about
+    render template: "about/index"
+  end
+end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root :to => 'products#index'
+  get 'home/home'
+  get 'about/index' 
+  root :to => 'home#home'
 
   resources :products do
     resources :reviews
