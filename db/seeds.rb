d5eb3446f3f3807a48e894f4af6d8c5b02c8b5f2
Starlynne <lewis.start0905@gmail.com>
2017-06-30 12:20:10 -0700

    Add Faker for 250 reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.destroy_all
+Review.destroy_all
+
+@products = []
 
 50.times do |index|
-  Product.create!(name: Faker::Food.unique.ingredient,
+
+  product = Product.create!(
+    name: Faker::Food.unique.ingredient,
     description: Faker::Lorem.sentence(20, false, 0).chop,
     cost: Faker::Commerce.price,
     country: Faker::Address.country)
+  @products.push(product.id)
+end
+
+250.times do |index|
+  Review.create!(
+    name: Faker::Name.name,
+    rating: Faker::Number.between(1, 5),
+    content_body: Faker::StarWars.quote,
+    product_id: Faker::Number.between(@products[0],@products[49])
+  )
 end
 
 p "Created #{Product.count} products"
